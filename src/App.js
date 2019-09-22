61184e3a32a11a2ed5104e3ac56bf061d22eedc6
Shane <shanemcneil19@gmail.com>
2019-09-22 16:58:45 -0700

    create Search component

--- src/App.js
+++ src/App.js
@@ -12,39 +12,6 @@ export default class App extends Component {
     };
   }
 
-  componentDidMount() {
-    const key = "AIzaSyAcbVYDqYMoQ4oWJP-O2u_0Xf4j3rLtWPs";
-    const url = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
-    const options = {
-      method: 'GET',
-      headers: {
-        "key": key,
-        "Content-type": "application/json"
-      }
-    };
-
-    fetch(url, options)
-      .then(res => {
-        if(!res.ok) {
-          throw new Error('Something went wrong, please try again later.');
-        }
-        return res;
-      })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        this.setState({
-          books: data,
-          error: null
-        });
-      })
-      .catch(err => {
-        this.setState({
-          error: err.message
-        });
-      });
-    }
-
   render() {
     return (
       <div className="App">
@@ -53,7 +20,8 @@ export default class App extends Component {
         </header>
         <Search />
         <Filter />
-        <BookList />
+        <BookList 
+          books={this.state.books}/>
       </div>
     );
   }
